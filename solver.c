0725ba3fc3f88819c7cb3b2feb67555b40227fa0
Matias Jokela <mjokela@c2r5p6.hive.fi>
2022-01-05 18:17:28 +0200

    basic functionality seems to work, recursive solver next

--- solver.c
+++ solver.c
@@ -24,12 +24,24 @@ char **solve_map(int **bin_arr, int piece_count)
 	while (side_len * side_len < piece_count * 4)
 		side_len++;
 	give_map(map, side_len);
-	bin_arr[1][0] = bin_arr[1][0] << 5;
-	move_piece(bin_arr, map, 1, side_len);
 	plant_piece(bin_arr, map, 1, side_len);
-	for (int i = 0; i < 15; i++)
-		print_bits(map[i]);	
+	while (move_piece(bin_arr, map, 1, side_len))
+	{
+		plant_piece(bin_arr, map, 1, side_len);
+		for (int i = 0; i < 15; i++)
+			print_bits(map[i]);
+		//sleep(1);
+		printf("\n");
+	}
+	restore_piece(bin_arr, 1);
+	pop_piece(map, bin_arr, 0);
+
+	printf("\n");
+	for (int i = 0; i < 26; i++)
+		print_bits(map[i]);
 	/*for (int i = 0; i < 15; i++)
+		print_bits(map[i]);	
+	for (int i = 0; i < 15; i++)
 		print_bits(map[i]);
 	bin_arr[1][0] = bin_arr[1][0] << 1;
 	printf("plant piece returns: %d\n", plant_piece(bin_arr, map, 1, side_len));
@@ -46,10 +58,12 @@ char **solve_map(int **bin_arr, int piece_count)
 returns 1 if piece can be planted and 0 if not*/
 int	plant_piece(int **bin_arr, int *map, int i, int side_len)
 {
-	int j;
+	int	j;
+	int	b;
 
 	j = 0;
-	while (j < side_len)
+	b = 1;
+	/*while (j < side_len)
 	{
 		if ((map[j] & bin_arr[i][j]) != 0)
 		{
@@ -60,13 +74,23 @@ int	plant_piece(int **bin_arr, int *map, int i, int side_len)
 		}
 		j++;
 	}
-	j = 0;
+	j = 0;*/
 	while (j < side_len)
 	{
-		map[j] += bin_arr[i][j];
+		if ((map[j] & bin_arr[i][j]) != 0)
+			b = 0;
 		j++;
-	}	
-	return (1);
+	}
+	if (b == 1)
+	{
+		j = 0;
+		while (j < side_len)
+		{
+			map[j] += bin_arr[i][j];
+			j++;
+		}	
+	}
+	return (b);
 }
 
 void	give_map(int *map, int side_len)
@@ -97,7 +121,9 @@ int	move_piece(int **bin_arr, int *map, int i, int side_len)
 	{
 		// Check if piece is out of bounds without moving it yet
 		if ((bin_arr[i][j] << 1 & mask) != 0)
-			move_to_next_row(bin_arr, i);
+			return (move_to_next_row(bin_arr, i, side_len));
+		else
+			bin_arr[i][j] = bin_arr[i][j] << 1;
 		j++;
 	}
 	/*
@@ -107,38 +133,68 @@ int	move_piece(int **bin_arr, int *map, int i, int side_len)
 	return (1);
 }
 
-void	move_to_next_row(int **bin_arr, int i)
+int	move_to_next_row(int **bin_arr, int i, int side_len)
 {
 	int	a;
-	int	b;
+	int b;
 
+	a = 0;
 	b = 0;
-	// Move piece to beginning of row
-	while (b == 0)
+
+	
+	while (a < 20)
 	{
-		a = 0;
-		while (a < 19)
+		if (bin_arr[i][a] != 0)
 		{
-			bin_arr[i][a] = bin_arr[i][a] >> 1;
-			if ((bin_arr[i][a] & 1) == 1)
-				b = 1;
-			a++;
+			bin_arr[i][a] = 0;
+			if (a + 1 < 20)
+				bin_arr[i][a + 1] = bin_arr[i][20];
+			if (a + 2 < 20)
+				bin_arr[i][a + 2] = bin_arr[i][21];
+			if (a + 3 < 20)
+				bin_arr[i][a + 3] = bin_arr[i][22];
+			if (a + 4 < 20)
+				bin_arr[i][a + 4] = bin_arr[i][23];
+			break ;
 		}
+		a++;
 	}
-	// Move piece one row down
-	while (a > 0)
-	{
-		bin_arr[i][a] = bin_arr[i][a - 1];
-		a--;
-	}
-	bin_arr[i][a] = 0;
+
+	if (bin_arr[i][side_len] != 0)
+		return (0);
+	return (1);
+
 }
 
-/*
-void	restore_piece()
-{
 
-}*/
+void	restore_piece(int **bin_arr, int i)
+{
+	int	a;
+
+	a = 0;
+	while (a < 4)
+	{
+		bin_arr[i][a] = bin_arr[i][a + 20];
+		a++;
+	}
+	while (a < 20)
+	{
+		bin_arr[i][a] = 0;
+		a++;
+	}	
+}
+
+void	pop_piece(int *map, int **bin_arr, int i)
+{
+	int a;
+
+	a = 0;
+	while (a < 20)
+	{
+		map[a] -= bin_arr[i][a];
+		a++;
+	}
+}
 
 void	print_bits(int n)
 {
