8ec1e170c400d8c30ad9544b367aacf00e7f7942
matiasjokela <matias.jokela@outlook.com>
2023-06-11 20:10:04 +0300

    Colors added

--- end_game.c
+++ end_game.c
@@ -49,7 +49,7 @@ void	fill_solved_map(int **bin_arr, int side_len, char solution[][14])
 			{
 				if ((bin_arr[i][j] & 1) == 1)
 					solution[j][k] = i + 'A';
-				bin_arr[i][j] = bin_arr[i][j] >> 1;
+				bin_arr[i][j] = bin_arr[i][j] >> 1; // This is why the print doesn't work
 				k++;
 			}
 			j++;
@@ -66,12 +66,45 @@ void	print_map(int side_len, char solution[][14])
 	int	j;
 
 	i = 0;
+	char *colors[26] = {
+		"\033[31m", // Red
+		"\033[32m", // Green
+		"\033[33m", // Yellow
+		"\033[34m", // Blue
+		"\033[35m", // Magenta
+		"\033[36m", // Cyan
+		"\033[1;31m", // Bold Red
+		"\033[1;32m", // Bold Green
+		"\033[1;33m", // Bold Yellow
+		"\033[1;34m", // Bold Blue
+		"\033[1;35m", // Bold Magenta
+		"\033[1;36m", // Bold Cyan
+		"\033[37m", // White
+		"\033[30m", // Black
+		"\033[90m", // Bright Black
+		"\033[91m", // Bright Red
+		"\033[92m", // Bright Green
+		"\033[93m", // Bright Yellow
+		"\033[94m", // Bright Blue
+		"\033[95m", // Bright Magenta
+		"\033[96m", // Bright Cyan
+		"\033[97m", // Bright White
+		"\033[1;37m", // Bold White
+		"\033[1;30m",  // Bold Black
+		"\033[31m", // Red
+		"\033[32m" // Green
+	};
+
 	while (i < side_len)
 	{
 		j = 0;
+
 		while (j < side_len)
 		{
+			if (solution[i][j] != '.')
+				ft_putstr(colors[solution[i][j] - 'A']);
 			ft_putchar(solution[i][j]);
+			ft_putstr("\033[0m");
 			j++;
 		}
 		ft_putchar('\n');
